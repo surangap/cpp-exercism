46d274429a33350a5caf1a018d0666b132338966
surangap <19677661+surangap@users.noreply.github.com>
2024-06-01 23:56:39 +1200

    Add space-age sol

--- exercises/practice/space-age/space_age.cpp
+++ exercises/practice/space-age/space_age.cpp
@@ -2,6 +2,46 @@
 
 namespace space_age {
 
-// TODO: add your solution here
 
-}  // namespace space_age
+space_age::space_age(long secs) {
+    secs_ = secs;
+}
+
+int space_age::seconds() const {
+    return secs_;
+}
+
+double space_age::on_earth() const {
+    return secs_/EARTH_YEAR_IN_SECS;
+}
+
+double space_age::on_mercury() const {
+    return secs_/EARTH_YEAR_IN_SECS/MERCURY_OP;
+}
+
+double space_age::on_venus() const {
+    return secs_/EARTH_YEAR_IN_SECS/VENUS_OP;
+}
+
+double space_age::on_mars() const {
+    return secs_/EARTH_YEAR_IN_SECS/MARS_OP;
+}
+
+double space_age::on_jupiter() const {
+    return secs_/EARTH_YEAR_IN_SECS/JUPITER_OP;
+}
+
+double space_age::on_saturn() const {
+    return secs_/EARTH_YEAR_IN_SECS/SATURN_OP;
+}
+
+double space_age::on_uranus() const {
+    return secs_/EARTH_YEAR_IN_SECS/URANUS_OP;
+}
+
+double space_age::on_neptune() const {
+    return secs_/EARTH_YEAR_IN_SECS/NEPTUNE_OP;
+}
+
+}
+// namespace space_age
--- exercises/practice/space-age/space_age.h
+++ exercises/practice/space-age/space_age.h
@@ -1,6 +1,33 @@
 #pragma once
 
 namespace space_age {
+const int EARTH_YEAR_IN_SECS = 31557600;
+const double MERCURY_OP = 0.2408467;
+const double VENUS_OP = 0.61519726;
+const double MARS_OP = 1.8808158;
+const double JUPITER_OP = 11.862615;
+const double SATURN_OP = 29.447498;
+const double URANUS_OP = 84.016846 ;
+const double NEPTUNE_OP = 164.79132;
+
+
+
+class space_age {
+    public:
+        space_age(long secs);
+        int seconds() const;
+        double on_earth() const;
+        double on_mercury() const;
+        double on_venus() const;
+        double on_mars() const;
+        double on_jupiter() const;
+        double on_saturn() const;
+        double on_uranus() const;
+        double on_neptune() const;
+
+    private:
+        double secs_;
+};
 
 // TODO: add your solution here
 
